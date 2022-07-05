4dde3aa1f16b6332df3b2a09b3d92fa5409838bb
emeraldgoose <emeraldgoose@gmail.com>
2022-07-05 12:45:33 +0000

    MSELoss 수정

--- hcrot/layers.py
+++ hcrot/layers.py
@@ -42,12 +42,12 @@ class MSELoss:
     def __call__(self, y_pred, y_true):
         self.one_hot_enc = [[0 for _ in range(len(y_pred[0]))] for _ in range(len(y_pred))]
         for i in range(len(y_true)): self.one_hot_enc[i][y_true[i]] = 1
-        sum_ = [sum([(a-b)**2 for a,b in zip(y_pred[i], self.one_hot_enc[i])])/len(y_pred[0]) for i in range(len(y_true))]
+        sum_ = [sum([(a-b)**2 for a,b in zip(y_pred[i], self.one_hot_enc[i])]) for i in range(len(y_true))]
         return sum(sum_)/len(y_true)
 
     def backward(self, y_pred):
         batch = len(y_pred)
-        return [sum([a-b for a,b in zip(y_pred[i], self.one_hot_enc[i])]) / batch for i in range(batch)]
+        return [[2*(y_pred[i][j]-self.one_hot_enc[i][j]) / batch for j in range(len(y_pred[i]))] for i in range(len(y_pred))]
 
 class CrossEntropyLoss:
     def __call__(self, y_pred, y_true):
