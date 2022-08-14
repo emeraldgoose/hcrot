730b45493f615d88d0f64022d34a75ac9a807187
emeraldgoose <emeraldgoose@gmail.com>
2022-08-14 05:52:26 +0000

    fix : 초기화 수정, Pad 함수 인자 추가, backward 수정

--- hcrot/layers/conv.py
+++ hcrot/layers/conv.py
@@ -1,30 +1,30 @@
 from hcrot.utils import *
-import numpy as np
 
 class Conv2d:
     def __init__(self, in_channel, out_channel, kernel, stride=1, padding=0):
         self.in_channel = in_channel
         self.out_channel = out_channel
         # default group = 1, dilation = 1
-        # k = 1/(in_channel*sum(kernel))
         if type(kernel) == tuple:
-            self.weight = np.random.randn(out_channel, in_channel, kernel[0], kernel[1])
+            k = 1/(in_channel*sum(kernel))
+            self.weight = np.random.uniform(-np.sqrt(k),np.sqrt(k),(out_channel, in_channel, kernel[0], kernel[1]))
         else:
-            self.weight = np.random.randn(out_channel, in_channel, kernel, kernel)
-        self.bias = np.random.randn(out_channel)
-        
+            k = 1/(in_channel*(kernel*2))
+            self.weight = np.random.uniform(-np.sqrt(k),np.sqrt(k),(out_channel, in_channel, kernel, kernel))
+        self.bias = np.random.uniform(-np.sqrt(k), np.sqrt(k), out_channel)
+
         if type(stride) == int: self.stride = (stride, stride)
         else: self.stride = stride
         
         if type(padding) == int: self.padding = (padding,padding)
         else: self.padding = padding
         self.X, self.Z = None, None
-
+    
     def __call__(self, x):
         # original image shape (B, H, W, C) -> converted (B, C, H, W)
-        pad_x = self.Pad(x)
-        self.X = pad_x
-        image, kernel, batch = x[0][0], self.weight[0][0], len(x)
+        self.X = x
+        pad_x = self.Pad(x, self.padding)
+        image, kernel, batch = pad_x[0][0], self.weight[0][0], len(x)
         hin, win = len(image), len(image[0])
         hout = math.floor((hin + 2 * self.padding[0] - 1 * (len(kernel)-1) - 1) / self.stride[0] + 1)
         wout = math.floor((win + 2 * self.padding[1] - 1 * (len(kernel[0])-1) - 1) / self.stride[1] + 1)
@@ -48,13 +48,13 @@ class Conv2d:
         xpos, ypos = pos # input pos
         return sum([input[xpos+i][ypos+j] * kernel[i][j] for j in range(len(kernel[0])) for i in range(len(kernel))])
 
-    def Pad(self, x):
+    def Pad(self, x, padding):
         # (C, H, W)
         B, C, H, W = len(x), len(x[0]), len(x[0][0]), len(x[0][0][0])
-        ret = np.zeros((B,C,H+self.padding[0]*2,W+self.padding[1]*2))
+        ret = np.zeros((B,C,H+padding[0]*2,W+padding[1]*2))
         for b in range(B):
             for c in range(C):
-                ret[b][c] = np.pad(x[b][c],((self.padding[0],self.padding[0]),(self.padding[1],self.padding[1])))
+                ret[b][c] = np.pad(x[b][c],((padding[0],padding[0]),(padding[1],padding[1])))
         return ret
 
     def backward(self, dout):
@@ -74,4 +74,24 @@ class Conv2d:
                     dw[o][i] += res
             for o in range(out_channel): db[o] += np.sum(dout[b][o])
 
-        return dw/batch, db/batch
+        # dz need 0-pad (1,1), dx = convolution(dz, weight)
+        pad_h, pad_w = len(self.weight[0][0])-1, len(self.weight[0][0][0])-1
+        dout = self.Pad(dout,(pad_h,pad_w))
+        
+        dz = np.zeros_like(self.X)
+        hout, wout = len(dz[0][0]), len(dz[0][0][0])
+
+        for b in range(batch):
+            for cout in range(out_channel):
+                for cin in range(in_channel):
+                    res = np.zeros((hout, wout))
+                    flip_w = np.flip(self.weight[cout][cin])
+                    # convolution
+                    for x in range(hout-pad_h):
+                        for y in range(wout-pad_w):
+                            res[x+pad_h][y+pad_w] = self.position_sum((x,y), flip_w, dout[b][cin])
+                    dz[b][cin] += res
+
+        # remove pad
+        dz = dz[:,:,pad_h:-pad_h,pad_w:-pad_w]
+        return dw/batch, db/batch, dz
