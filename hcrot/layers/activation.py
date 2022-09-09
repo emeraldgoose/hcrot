80717640e4533683c61f128a1534e3eae3259f0c
emeraldgoose <emeraldgoose@gmail.com>
2022-09-09 05:33:55 +0000

    fix: deriv -> backward 통일

--- hcrot/layers/activation.py
+++ hcrot/layers/activation.py
@@ -7,7 +7,7 @@ class Softmax:
         self.sum_ = [sum([exp**i for i in inputs[j]]) for j in range(len(inputs))]
         return [[exp**i/self.sum_[j] for i in inputs[j]] for j in range(len(inputs))]
     
-    def deriv(self, inputs):
+    def backward(self, inputs):
         e_a = [[exp**self.X[i][j] for j in range(len(inputs[0]))] for i in range(len(inputs))]
         return [[((1+inputs[i][j])/self.sum_[i])*e_a[i][j] for j in range(len(e_a[0]))] for i in range(len(e_a))]
 
@@ -16,7 +16,7 @@ class Sigmoid:
         ret = [[1/(1+(exp**(-x))) for x in inputs[i]] for i in range(len(inputs))]      
         return ret
 
-    def deriv(self, inputs):
+    def backward(self, inputs):
         x = self(inputs)
         return [[x[i][j]*(1-x[i][j]) for j in range(len(x[0]))] for i in range(len(x))]
 
--- hcrot/optim.py
+++ hcrot/optim.py
@@ -11,8 +11,6 @@ class Optimizer:
             if module.__class__.__name__ == "Sigmoid":
                 dsig = module.deriv(self.modules[i-1].Z)
                 dz = [[a*b for a,b in zip(dsig[i],dz[i])] for i in range(len(dz))]
-            elif module.__class__.__name__ == "Softmax":
-                dz = module.deriv(dz)
             elif module.__class__.__name__ == "Linear":
                 dw, db = module.backward(dz)
                 dz = dot_numpy(dz,transpose(module.weight))
@@ -25,9 +23,5 @@ class Optimizer:
               dw, db, dz = module.backward(dz)
               module.weight -= self.lr_rate * dw
               module.bias -= self.lr_rate * db
-            elif module.__class__.__name__ == "Flatten":
-              dz = module.backward(dz)
-            elif module.__class__.__name__ == "MaxPool2d":
-              dz = module.backward(dz)
-            elif module.__class__.__name__ == "ReLU":
+            else:
               dz = module.backward(dz)
