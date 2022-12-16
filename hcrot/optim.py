e0cc76a15ddba2de5287c66e3229a4f55617c413
emeraldgoose <emeraldgoose@gmail.com>
2022-12-16 16:11:58 +0900

    optimizer update rnn and add comment

--- hcrot/optim.py
+++ hcrot/optim.py
@@ -2,6 +2,7 @@ from typing import Any, Tuple
 from hcrot.utils import *
 
 class Optimizer:
+    """Gradient Descent"""
     def __init__(self, Net: Any, lr_rate: float):
         self.modules = Net.sequential
         self.lr_rate = lr_rate
@@ -19,6 +20,14 @@ class Optimizer:
                 dw, db, dz = module.backward(dz)
                 module.weight = self.weight_update(f'{id(module)}_weight', module.weight, dw, self.lr_rate)
                 module.bias = self.weight_update(f'{id(module)}_bias', module.bias, db, self.lr_rate)
+            elif module.__class__.__name__ == "RNN":
+                dz, dw, db = module.backward(dz)
+                for k,v in dw.items():
+                    new_weight = self.weight_update(k, module.__getattribute__(k), v, self.lr_rate)
+                    module.__setattr__(k, new_weight)
+                for k, v in db.items():
+                    new_bias = self.weight_update(k, module.__getattribute__(k), v, self.lr_rate)
+                    module.__setattr__(k, new_bias)
             else:
                 dz = module.backward(dz)
     
@@ -34,6 +43,7 @@ class Optimizer:
         return dict(w+b)
 
 class SGD(Optimizer):
+    """Stochastic Gradient Descent"""
     def __init__(self, Net: Any, lr_rate: float, momentum: float = 0.9):
         super().__init__(Net, lr_rate)
         self.momentum = momentum
@@ -47,6 +57,7 @@ class SGD(Optimizer):
         return weight + self.v[f'{id}']
 
 class Adam(Optimizer):
+    """Adaptive moment estimation"""
     def __init__(self, Net: Any, lr_rate: float, betas: Tuple[float, float] = (0.9, 0.999), eps: float = 1e-8):
         super().__init__(Net, lr_rate)
         self.betas = betas
