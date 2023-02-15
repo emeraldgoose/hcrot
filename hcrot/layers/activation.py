fix Softmax layer

hcrot/layers/activation.py
@@ -6,14 +6,16 @@ class Softmax(Module):
         return self.forward(x)
 
     def forward(self, x: np.ndarray):
+        if x.ndim >= 3:
+            raise ValueError('not possible backward() for dimension >= 3')
         self.X = x
         self.sum_ = np.sum(np.exp(x),axis=1)
         return softmax(x)
     
     def backward(self, dz: np.ndarray):
-        e = np.exp(self.X)
-        r = np.divide((1+dz).T,self.sum_).T
-        return r * e
+        s = softmax(self.X)
+        j = np.einsum('ij,jk->ijk', s, np.eye(s.shape[-1])) - np.einsum('ij,ik->ijk', s, s)
+        return np.einsum('ijk,ij->ik', j, dz)
 
 class Sigmoid(Module):
     def __call__(self, x: np.ndarray):

hcrot/utils.py
@@ -2,13 +2,17 @@ import numpy as np
 
 def one_hot_encoding(x: np.ndarray, y: np.ndarray):
     ret = np.zeros_like(x)
-    ret[np.arange(y.size),y]=1
+    ret[np.arange(y.size), y]=1
     return ret
 
 def softmax(x: np.ndarray):
-    sum_: np.ndarray = np.sum(np.exp(x),axis=1)
+    axis = _get_softmax_axis(x.ndim)
+    sum_ = np.sum(np.exp(x), axis=axis)
     sum_ += np.full(sum_.shape, 1e-7)
     return np.array(np.exp(x).T / sum_).T
+
+def _get_softmax_axis(ndim: int):
+    return 0 if ndim in [0,1,3] else 1
 
 def convolve2d(a: np.ndarray, f: np.ndarray):
     # Ref: https://stackoverflow.com/a/43087771