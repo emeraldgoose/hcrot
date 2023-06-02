46396d2b74482170ad5aa4845a872d9ff93bee02
emeraldgoose <emeraldgoose@gmail.com>
2023-06-02 14:10:51 +0900

    lstm implementatioin

--- hcrot/layers/rnn.py
+++ hcrot/layers/rnn.py
@@ -1,4 +1,5 @@
 from .module import Module
+from ..utils import sigmoid
 from numpy.typing import NDArray
 from typing import Any, Tuple, Mapping
 import numpy as np
@@ -27,6 +28,7 @@ class RNNBase(Module):
             ValueError(f'Unrecognized RNN mode: {mode}')
         
         for k in range(self.num_layers):
+            self.param_names += [f'weight_ih_l{k}', f'weight_hh_l{k}', f'bias_ih_l{k}', f'bias_hh_l{k}']
             if not k:
                 setattr(self, f'weight_ih_l{k}', np.zeros((gate_size, self.input_size)))
             else:
@@ -34,7 +36,6 @@ class RNNBase(Module):
             setattr(self, f'weight_hh_l{k}', np.zeros((gate_size, self.hidden_size)))
             setattr(self, f'bias_ih_l{k}', np.zeros((gate_size,)))
             setattr(self, f'bias_hh_l{k}', np.zeros((gate_size,)))
-            self.param_names += [f'weight_ih_l{k}', f'weight_hh_l{k}', f'bias_ih_l{k}', f'bias_hh_l{k}']
         
         self.reset_parameters()
 
@@ -81,69 +82,58 @@ class RNN(RNNBase):
             )
         self.nonlinearity = nonlinearity
         self.X = []
-        self.hs = None
-        self.h0 = None
         self.relu_mask = []
 
-    def __call__(self, x: NDArray, h0: NDArray = None):
-        return self.forward(x, h0)
+    def __call__(self, x: NDArray, h_0: NDArray = None):
+        return self.forward(x, h_0)
 
-    def forward(self, x: NDArray, h0: NDArray = None) -> Tuple[NDArray, NDArray]:
-        """
-        RNN forward process
-        (input_length or input_time_length, batch_size, input_features) = (L, B, F)
-        forward function:
-            - h_t = tanh(x_t @ W_{ih}.T + b_{ih} + h_{t-1} @ W_{hh}.T + b_{hh})
-            - https://pytorch.org/docs/stable/generated/torch.nn.RNN.html#torch.nn.RNN
-        """
+    def forward(self, x: NDArray, h_0: NDArray = None) -> Tuple[NDArray, NDArray]:
+        """RNN forward process"""
+        self.X = []
         if self.batch_first:
             x = np.transpose(x, (1,0,2))
-        (L, B, _), H = x.shape, self.hidden_size
+        (T, B, _), H = x.shape, self.hidden_size
 
         if self.nonlinearity == 'relu':
-            self.relu_mask = np.zeros((self.num_layers, L, B, H))
+            self.relu_mask = np.zeros((self.num_layers, T, B, H))
+        
+        if h_0 == None:
+            h_0 = np.zeros((self.num_layers, B, H))
 
-        hn = np.zeros((self.num_layers, B, H)) if h0 == None else h0 # (num_layers, B, H)
-        self.h0 = hn.copy() # deep copy
-        self.hs = np.zeros((self.num_layers, L, B, H)) # (num_layers, L, B, H)
-        out = np.zeros((L, B, H)) # (L, B, H)
-        self.X = []
+        self.h = [{-1:h_0[i]} for i in range(self.num_layers)]
+        out = np.zeros((T, B, H))
         
         for l in range(self.num_layers):
-            x = x if l == 0 else out # (L, B, F)
+            x = x if l == 0 else out
             self.X.append(x)
-            h_t = hn[l]
             wih, whh = getattr(self, f'weight_ih_l{l}'), getattr(self, f'weight_hh_l{l}')
             bih, bhh = getattr(self, f'bias_ih_l{l}'), getattr(self, f'bias_hh_l{l}')
             
-            for t in range(L):
-                hs_t = np.dot(x[t], wih.T) + bih + np.dot(h_t, whh.T) + bhh
+            for t in range(T):
+                hs_t = np.dot(x[t], wih.T) + bih + np.dot(self.h[l][t-1], whh.T) + bhh
                 
                 if self.nonlinearity == 'tanh':
-                    h_t = np.tanh(hs_t)
+                    self.h[l][t] = np.tanh(hs_t)
                 else:
                     self.relu_mask[l][t] = hs_t > 0
-                    h_t = self.relu_mask[l][t] * hs_t
+                    self.h[l][t] = self.relu_mask[l][t] * hs_t
                 
-                out[t] = h_t
-                self.hs[l][t] = h_t.copy()
-            
-            hn[l] = h_t
-
+                out[t] = self.h[l][t]
+                
         if self.batch_first:
             out = np.transpose(out, (1,0,2))
         
-        return out, hn
+        return out, self.h[l]
 
     def backward(self, dz: NDArray) -> Tuple[NDArray, Mapping[str, NDArray], Mapping[str, NDArray]]:
         """RNN backward process"""
-        if self.batch_first and len(dz.shape) == 3:
+        if self.batch_first and dz.ndim == 3:
             dz = np.transpose(dz, (1,0,2))
         
         dw, db, dx = {}, {}, dz
         for l in reversed(range(self.num_layers)):
-            dhnext = np.zeros(self.hs[l][0].shape) # (L, B, H)
-            dx, dwih, dwhh, dbih, dbhh = self._layer_backward(l, dhnext, dx)
+            dhnext = np.zeros(self.h[l][0].shape) # (L, B, H)
+            dx, dwih, dwhh, dbih, dbhh = self.__backward(l, dhnext, dx)
             dw[f'weight_ih_l{l}'] = dwih
             dw[f'weight_hh_l{l}'] = dwhh
             db[f'bias_ih_l{l}'] = dbih
@@ -154,7 +144,7 @@ class RNN(RNNBase):
         
         return dx, dw, db
 
-    def _layer_backward(self, layer: int, dhnext: NDArray, dz: NDArray) -> Tuple[NDArray, NDArray, NDArray, NDArray, NDArray]:
+    def __backward(self, layer: int, dhnext: NDArray, dz: NDArray) -> Tuple[NDArray, NDArray, NDArray, NDArray, NDArray]:
         """RNN layer backward process"""
         T = len(self.X[layer])
         wih, whh = getattr(self, f'weight_ih_l{layer}'), getattr(self, f'weight_hh_l{layer}')
@@ -165,18 +155,13 @@ class RNN(RNNBase):
         dx = np.zeros_like(self.X[layer])
 
         for t in reversed(range(T)):
-            """
-            rnn cell backward process
-            (batch_size, hidden_size, input_features) = (B, H, F)
-            """
-            dhnext += dz[t] if len(dz.shape) == 3 else (dz if t == T - 1 else 0) # (B, H)
-            h_next, h_prev = self.hs[layer][t], self.hs[layer][t-1] if t > 0 else self.h0[layer] # (B, H)
-            xt = self.X[layer][t] # (B, F)
+            """rnn cell backward process"""
+            dh = dhnext + (dz[t] if dz.ndim == 3 else (dz if t == T - 1 else 0)) # (B, H)            
+            dhtanh = ((1 - self.h[layer][t] ** 2) if self.nonlinearity == 'tanh' else self.relu_mask[layer][t]) * dh # (B, H)
             
-            dhtanh: np.ndarray = ((1 - h_next ** 2) if self.nonlinearity == 'tanh' else self.relu_mask[layer][t]) * dhnext # (B, H)
             dx[t] = np.dot(dhtanh, wih) # (B, F)
-            dwih += np.dot(dhtanh.T, xt) # (H, F)
-            dwhh += np.dot(dhtanh.T, h_prev) # (H, H)
+            dwih += np.dot(dhtanh.T, self.X[layer][t]) # (H, F)
+            dwhh += np.dot(dhtanh.T, self.h[layer][t-1]) # (H, H)
             dbih += np.sum(dhtanh, axis=0) # (H, 1)
             dbhh += np.sum(dhtanh, axis=0) # (H, 1)
             dhnext = np.dot(dhtanh, whh) # (B, H)
@@ -184,23 +169,113 @@ class RNN(RNNBase):
         return dx, dwih, dwhh, dbih, dbhh
 
 class LSTM(RNNBase):
-    def __init__(self, input_size: int, hidden_size: int, num_layers: int = 1, batch_first: bool = False):
+    def __init__(
+            self, 
+            input_size: int, 
+            hidden_size: int, 
+            num_layers: int = 1, 
+            batch_first: bool = False
+            ) -> None:
         super().__init__(
+            mode='LSTM',
             input_size=input_size,
             hidden_size=hidden_size,
             batch_first=batch_first,
             num_layers=num_layers
             )
-        self.reset_parameters()
+        self.X = []
+
+    def __call__(self, x: NDArray, h0: NDArray = None, c0: NDArray = None):
+        return self.forward(x, h0, c0)
+
+    def forward(self, x: NDArray, h_0: NDArray = None, c_0: NDArray = None) -> Tuple[NDArray, NDArray, NDArray]:
+        """LSTM forward process"""
+        self.X = []
+        if self.batch_first:
+            x = np.transpose(x, (1,0,2))
+        (T, B, _), H = x.shape, self.hidden_size
+
+        if h_0 == None:
+            h_0 = np.zeros((self.num_layers, B, H))
+        if c_0 == None:
+            c_0 = np.zeros((self.num_layers, B, H))
         
-    def reset_parameters(self) -> None:
-        pass
+        self.h = [{-1:h_0[i]} for i in range(self.num_layers)]
+        self.c = [{-1:c_0[i]} for i in range(self.num_layers)]
 
-    def __call__(self, *args: Any, **kwds: Any) -> Any:
-        return self.forward(*args, **kwds)
+        self.i = np.zeros((self.num_layers, T, B, H))
+        self.f = np.zeros((self.num_layers, T, B, H))
+        self.g = np.zeros((self.num_layers, T, B, H))
+        self.o = np.zeros((self.num_layers, T, B, H))
+        out = np.zeros((T, B, H))
+        
+        for l in range(self.num_layers):
+            x = x if l == 0 else out
+            self.X.append(x)
+            w_ih, w_hh = getattr(self, f'weight_ih_l{l}'), getattr(self, f'weight_hh_l{l}')
+            b_ih, b_hh = getattr(self, f'bias_ih_l{l}'), getattr(self, f'bias_hh_l{l}')
 
-    def forward(self):
-        pass
+            for t in range(T):
+                tmp = np.dot(x[t], w_ih.T) + b_ih + np.dot(self.h[l][t-1], w_hh.T) + b_hh
+                self.i[l][t] = sigmoid(tmp[:, :H])
+                self.f[l][t] = sigmoid(tmp[:, H:H*2])
+                self.g[l][t] = np.tanh(tmp[:, H*2:H*3])
+                self.o[l][t] = sigmoid(tmp[:, H*3:])
+                self.c[l][t] = self.f[l][t] * self.c[l][t-1] + self.i[l][t] * self.g[l][t]
+                self.h[l][t] = self.o[l][t] * np.tanh(self.c[l][t])
+                out[t] = self.h[l][t]
 
-    def backward(self):
-        pass
+        if self.batch_first:
+            out = np.transpose(out, (1,0,2))
+        
+        return out, self.h, self.c
+    
+    def backward(self, dz: NDArray) -> Tuple[NDArray, Mapping[str, NDArray], Mapping[str, NDArray]]:
+        """LSTM backward process"""
+        if self.batch_first and dz.ndim == 3:
+            dz = np.transpose(dz, (1,0,2))
+        
+        dw, db, dx = {}, {}, dz
+        for l in reversed(range(self.num_layers)):
+            dhnext = np.zeros(self.h[l][0].shape)
+            dcnext = np.zeros(self.c[l][0].shape)
+            dx, dwih, dwhh, dbih, dbhh = self.__backward(l, dhnext, dcnext, dx)
+            dw[f'weight_ih_l{l}'] = dwih
+            dw[f'weight_hh_l{l}'] = dwhh
+            db[f'bias_ih_l{l}'] = dbih
+            db[f'bias_hh_l{l}'] = dbhh
+        
+        if self.batch_first:
+            dx = np.transpose(dx, (1,0,2))
+        
+        return dx, dw, db
+
+    def __backward(self, layer: int, dhnext: NDArray, dcnext: NDArray, dz: NDArray) -> Tuple[NDArray, NDArray, NDArray, NDArray, NDArray]:
+        """LSTM layer backward process"""
+        T = len(self.X[layer])
+        w_ih, w_hh = getattr(self, f'weight_ih_l{layer}'), getattr(self, f'weight_hh_l{layer}')
+        b_ih, b_hh = getattr(self, f'bias_ih_l{layer}'), getattr(self, f'bias_hh_l{layer}')
+        
+        dwih, dwhh = np.zeros_like(w_ih), np.zeros_like(w_hh)
+        dbih, dbhh = np.zeros_like(b_ih), np.zeros_like(b_hh)
+        dx = np.zeros_like(self.X[layer])
+        for t in reversed(range(T)):
+            """LSTM cell backward process"""
+            dh = dhnext + (dz[t] if dz.ndim == 3 else (dz if t == T - 1 else 0))
+            dc = dcnext + dh * self.o[layer][t] * (1 - np.tanh(self.c[layer][t])**2)
+            
+            di = dc * self.g[layer][t] * self.i[layer][t] * (1 - self.i[layer][t])
+            df = dc * self.c[layer][t-1] * self.f[layer][t] * (1 - self.f[layer][t])
+            dg = dc * self.i[layer][t] * (1 - self.g[layer][t]**2)
+            do = dh * np.tanh(self.c[layer][t]) * self.o[layer][t] * (1 - self.o[layer][t])
+            dgates = np.hstack((di, df, dg, do))
+
+            dx[t] = np.dot(dgates, w_ih)
+            dwih += np.dot(dgates.T, self.X[layer][t])
+            dwhh += np.dot(dgates.T, self.h[layer][t-1])
+            dbih += np.sum(dgates, axis=0)
+            dbhh += np.sum(dgates, axis=0)
+            dhnext = np.dot(dgates, w_hh)
+            dcnext = dc * self.f[layer][t]
+        
+        return dx, dwih, dwhh, dbih, dbhh
