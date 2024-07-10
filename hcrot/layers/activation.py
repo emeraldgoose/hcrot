f1ccd108dd551c5b11d919031d6a9e5f15bf527e
emeraldgoose <emeraldgoose@gmail.com>
2024-07-11 06:58:11 +0900

    fix: scaled_dot_product_attention_backward #12

--- hcrot/layers/activation.py
+++ hcrot/layers/activation.py
@@ -13,8 +13,8 @@ class Softmax(Module):
 
     def forward(self, x: NDArray) -> NDArray:
         e_x = np.exp(x - np.max(x, axis=self.dim, keepdims=True))
-        self.s = e_x / np.sum(e_x, axis=self.dim, keepdims=True)
-        return self.s
+        self.output = e_x / np.sum(e_x, axis=self.dim, keepdims=True)
+        return self.output
     
     def backward(self, dz: NDArray) -> NDArray:
         # powered by gpt-4o
@@ -24,7 +24,7 @@ class Softmax(Module):
         transposed_axes = list(range(dz.ndim))
         transposed_axes[self.dim], transposed_axes[-1] = transposed_axes[-1], transposed_axes[self.dim]
         transposed_dout = np.transpose(dz, transposed_axes)
-        transposed_softmax = np.transpose(self.s, transposed_axes)
+        transposed_softmax = np.transpose(self.output, transposed_axes)
         transposed_dx = np.transpose(dx, transposed_axes)
         
         # Get the shape of the transposed arrays
@@ -123,8 +123,11 @@ class MultiHeadAttention(Module):
             pass
 
         # variables
+        self.attn_mask = attn_mask
         tgt_len, bsz, embed_dim = query.shape
         src_len, _, _ = key.shape
+        
+        self.softmax = Softmax()
 
         # linear(query), linear(key), linear(value)
         q = query @ self.q_proj_weight.T + self.q_proj_bias
@@ -137,15 +140,13 @@ class MultiHeadAttention(Module):
         v = v.reshape(v.shape[0], bsz * self.num_heads, self.head_dim).transpose((1,0,2))
         
         # reshape (bsz, num_heads, length, head_dim)
-        q = q.reshape(bsz, self.num_heads, tgt_len, self.head_dim)
-        k = k.reshape(bsz, self.num_heads, src_len, self.head_dim)
-        v = v.reshape(bsz, self.num_heads, src_len, self.head_dim)
+        self.q = q.reshape(bsz, self.num_heads, tgt_len, self.head_dim)
+        self.k = k.reshape(bsz, self.num_heads, src_len, self.head_dim)
+        self.v = v.reshape(bsz, self.num_heads, src_len, self.head_dim)
 
         # calculate attention score
-        attn_output = self.scaled_dot_product_attention(q, k, v, attn_mask)
+        attn_output = self.scaled_dot_product_attention(self.q, self.k, self.v, self.attn_mask)
         attn_output = attn_output.transpose(2,0,1,3).reshape(bsz * tgt_len, embed_dim)
-        
-        # dropout
         
         attn_output = attn_output @ self.out_proj_weight.T + self.out_proj_bias
         attn_output = attn_output.reshape(tgt_len, bsz, attn_output.shape[1])
@@ -153,58 +154,21 @@ class MultiHeadAttention(Module):
         return attn_output
 
     def backward(self, dx: NDArray) -> Tuple[Mapping[str, NDArray], Mapping[str, NDArray]]:
-        """
-        역전파 메소드 구현. 이 메소드는 입력된 그래디언트에 대해 각 파라미터의 그래디언트를 계산합니다.
-        """
-        # 역전파를 위한 준비: 각 행렬의 전치 및 재배열
-        d_query, d_key, d_value = self.scaled_dot_product_attention_backward(dx)
-
-        # 각 투영 매트릭스에 대한 그래디언트 계산
-        grad_q_proj_weight = d_query.T @ self.q
-        grad_k_proj_weight = d_key.T @ self.k
-        grad_v_proj_weight = d_value.T @ self.v
-
-        # 바이어스에 대한 그래디언트 계산
-        grad_q_proj_bias = np.sum(d_query, axis=0)
-        grad_k_proj_bias = np.sum(d_key, axis=0)
-        grad_v_proj_bias = np.sum(d_value, axis=0)
-
-        # 출력 투영 매트릭스에 대한 그래디언트 계산
-        grad_out_proj_weight = dx.T @ self.attn_output
-        grad_out_proj_bias = np.sum(dx, axis=0)
-
-        # 그래디언트 딕��너리 생성
-        grads = {}
-        param_names = ['q_proj_weight', 'k_proj_weight', 'v_proj_weight', 'q_proj_bias', 'k_proj_bias', 'v_proj_bias', 'out_proj_weight', 'out_proj_bias']
-        grad_values = [grad_q_proj_weight, grad_k_proj_weight, grad_v_proj_weight, grad_q_proj_bias, grad_k_proj_bias, grad_v_proj_bias, grad_out_proj_weight, grad_out_proj_bias]
-
-        for name, value in zip(param_names, grad_values):
-            setattr(grads, name, value)
-
-        return grads, {}
-
-    def scaled_dot_product_attention_backward(self, dx: NDArray) -> Tuple[NDArray, NDArray, NDArray]:
-        """
-        스케일된 점곱 주의 메커니즘의 역전파를 계산합니다.
-        """
-        # 주의 가중치와 값에 대한 그래디언트 계산
-        d_attn_weight = dx @ self.v.swapaxes(-1, -2)
-        d_value = self.attn_weight.swapaxes(-1, -2) @ dx
-
-        # 쿼리와 키에 대한 그래디언트 계산
-        d_query = d_attn_weight @ self.k
-        d_key = self.q.swapaxes(-1, -2) @ d_attn_weight
-
-        return d_query, d_key, d_value
-    
-    @staticmethod
-    def scaled_dot_product_attention(query: NDArray, key: NDArray, value: NDArray, attn_mask: NDArray[np.bool_] = None) -> NDArray:
-        scaled_factor = 1 / math.sqrt(query.shape[-1])
-        attn_weight = query @ key.swapaxes(-1,-2) * scaled_factor
-        if attn_mask is not None:
-            attn_weight = masked_fill(attn_weight, attn_mask, -1e9)
-        attn_weight = softmax(x=attn_weight, dim=-1)
-        return attn_weight @ value
-    
-    def scaled_dot_product_attention_backward(self, dx: NDArray) -> Tuple[NDArray, NDArray, NDArray]:
         raise NotImplementedError
+    
+    def scaled_dot_product_attention(self, query: NDArray, key: NDArray, value: NDArray, attn_mask: NDArray[np.bool_] = None) -> NDArray:
+        self.scaled_factor = 1 / math.sqrt(query.shape[-1])
+        attn_weight = query @ key.swapaxes(-1,-2) * self.scaled_factor
+        if attn_mask is not None:
+            attn_weight = masked_fill(attn_weight, attn_mask, float('-inf'))
+        self.attn_weight = self.softmax(x=attn_weight) @ value
+        return self.attn_weight
+    
+    def scaled_dot_product_attention_backward(self, dz: NDArray) -> Tuple[NDArray, NDArray, NDArray]:
+        dW = dz @ self.v.swapaxes(-1,-2)
+        dA = self.softmax.backward(dW)
+        
+        dV = self.attn_weight.swapaxes(-1,-2) @ dz
+        dQ = (dA @ self.k) * self.scaled_factor
+        dK = np.einsum('...ik,...ij->...kj', dA, self.q)
+        return dQ, dK, dV
