f0370e83c1710885249d2f8bd593789722b5a132
emeraldgoose <emeraldgoose@gmail.com>
2024-07-11 06:56:03 +0900

    fix: class variables

--- hcrot/layers/loss.py
+++ hcrot/layers/loss.py
@@ -12,10 +12,10 @@ class MSELoss:
         self.B = y_pred.shape[0]
         self.enc = one_hot_encoding(y_pred, y_true)
         sum_ = np.sum((y_pred - self.enc)**2, axis=1)
-        return np.sum(sum_, axis=1) / self.B
+        return np.sum(sum_) / self.B
 
     def backward(self) -> NDArray:
-        return 2*(self.y_pred - self.enc) / self.B
+        return 2 * (self.y_pred - self.enc) / self.B
 
 class CrossEntropyLoss:
     def __call__(self, y_pred: NDArray, y_true: NDArray) -> NDArray:
@@ -24,12 +24,10 @@ class CrossEntropyLoss:
         return self.forward(y_pred, y_true)
 
     def forward(self, y_pred: NDArray, y_true: NDArray) -> NDArray:
-        self.y_pred = y_pred
         self.B = y_pred.shape[0]
         self.enc = one_hot_encoding(y_pred, y_true)
-        r = np.log(softmax(y_pred))
-        return np.sum(-r * self.enc) / self.B
+        self.s = softmax(y_pred)
+        return np.sum(-np.log(self.s) * self.enc) / self.B
 
     def backward(self) -> NDArray:
-        s = softmax(self.y_pred)
-        return (s - self.enc) / self.B
+        return (self.s - self.enc) / self.B
