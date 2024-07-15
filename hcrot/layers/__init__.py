c359efde744d3ae4f320d3001891976df13e52e8
emeraldgoose <emeraldgoose@gmail.com>
2024-07-15 15:52:22 +0900

    fix: GELU and add LayerNorm

--- hcrot/layers/__init__.py
+++ hcrot/layers/__init__.py
@@ -1,4 +1,4 @@
-from .activation import Softmax, Sigmoid, ReLU
+from .activation import Softmax, Sigmoid, ReLU, GELU
 from .layer import Linear, Flatten, Embedding, Dropout
 from .loss import MSELoss, CrossEntropyLoss
 from .conv import Conv2d
@@ -9,7 +9,7 @@ from .module import Module, Sequential
 from .norm import LayerNorm
 
 __all__ = [
-    'Softmax', 'Sigmoid', 'ReLU',
+    'Softmax', 'Sigmoid', 'ReLU', 'GELU'
     'Linear', 'Flatten', 'Embedding', 'Dropout',
     'MSELoss', 'CrossEntropyLoss',
     'Conv2d',
