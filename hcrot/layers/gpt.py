dfecbcd5c7e5fdd14d2a030a15e05d8c6f3bd984
emeraldgoose <emeraldgoose@gmail.com>
2026-01-06 20:52:25 +0900

    fix: comment and var

--- hcrot/layers/gpt.py
+++ hcrot/layers/gpt.py
@@ -12,66 +12,65 @@ from .activation import MultiHeadAttention, GELU
 from hcrot.utils import *
 
 class GPTEmbedding(Module):
-    def __init__(self, vocab_size, embed_dim, max_len):
+    def __init__(self, vocab_size: int, embed_dim: int, max_len: int):
         super().__init__()
         self.token_embed = Embedding(vocab_size, embed_dim)
         self.pos_embed = Embedding(max_len, embed_dim)
     
-    def forward(self, x):
+    def forward(self, x: NDArray) -> NDArray:
         bsz, seq = x.shape
 
-        positions = np.arange(0, seq) # (seq, )
         tok = self.token_embed(x) # (bsz, seq, embed_dim)
-        pos = self.pos_embed(positions) # (bsz, seq)
+        
+        positions = np.arange(0, seq) # (seq, )
+        pos = self.pos_embed(positions) # (seq, embed_dim)
 
         return tok + pos[None, :, :] # (bsz, seq, embed_dim)
 
-    def backward(self, dz):
+    def backward(self, dz: NDArray) -> Dict[str, NDArray]:
         dw = {}
         _, token_embed_dw = self.token_embed.backward(dz)
         dw["token_embed.weight"] = token_embed_dw
 
-        dz = np.sum(dz, axis=0) # (bsz, seq)
-        _, pos_embed_dw = self.pos_embed.backward(dz)
+        dpos = np.sum(dz, axis=0) # (seq, embed_dim)
+        _, pos_embed_dw = self.pos_embed.backward(dpos)
         dw["pos_embed.weight"] = pos_embed_dw
         return dw
 
 class FeedForward(Module):
-    def __init__(self, embed_dim):
+    def __init__(self, embed_dim: int):
         super().__init__()
         self.fc = Linear(embed_dim, 4 * embed_dim)
         self.actv = GELU()
         self.fc2 = Linear(4 * embed_dim, embed_dim)
     
-    def forward(self, x):
+    def forward(self, x: NDArray) -> NDArray:
         x = self.fc(x)
         x = self.actv(x)
         x = self.fc2(x)
         return x
     
-    def backward(self, dz):
+    def backward(self, dz: NDArray) -> Tuple[NDArray, Dict[str, NDArray], Dict[str, NDArray]]:
         dw, db = {}, {}
         dx, fc2_dw, fc2_db = self.fc2.backward(dz)
-        dw["fc2.weight"] = fc2_dw
-        db["fc2.bias"] = fc2_db
+        dw["fc2.weight"], db["fc2.bias"] = fc2_dw, fc2_db
         
         dx = self.actv.backward(dx)
         
         dx, fc_dw, fc_db = self.fc.backward(dx)
-        dw["fc.weight"] = fc_dw
-        db["fc.bias"] = fc_db
+        dw["fc.weight"], db["fc.bias"] = fc_dw, fc_db
 
         return dx, dw, db
 
 class GPTBlock(Module):
-    def __init__(self, embed_dim, num_heads):
+    def __init__(self, embed_dim: int, num_heads: int):
         super().__init__()
         self.ln1 = LayerNorm(embed_dim)
         self.attn = MultiHeadAttention(embed_dim, num_heads, batch_first=True)
         self.ln2 = LayerNorm(embed_dim)
         self.ff = FeedForward(embed_dim)
 
-    def forward(self, x):
+    def forward(self, x: NDArray) -> NDArray:
         bsz, seq, dim = x.shape
         norm_x = self.ln1(x)
 
@@ -81,7 +80,7 @@ class GPTBlock(Module):
         x = x + self.ff(self.ln2(x))
         return x
     
-    def backward(self, dz):
+    def backward(self, dz: NDArray) -> Tuple[NDArray, Dict[str, NDArray], Dict[str, NDArray]]:
         dw, db = {}, {}
         dx_, ff_dw, ff_db = self.ff.backward(dz)
         for k, v in ff_dw.items():
@@ -91,8 +90,7 @@ class GPTBlock(Module):
             db[f"ff.{k}"] = v
 
         dx_, ln2_dw, ln2_db = self.ln2.backward(dx_)
-        dw["ln2.weight"] = ln2_dw
-        db["ln2.bias"] = ln2_db
+        dw["ln2.weight"], db["ln2.bias"] = ln2_dw, ln2_db
         
         dz = dx_ + dz
         
@@ -105,8 +103,7 @@ class GPTBlock(Module):
             db[f"attn.{k}"] = v
         
         dx_, ln1_dw, ln1_db = self.ln1.backward(dx_)
-        dw["ln1.weight"] = ln1_dw
-        db["ln1.bias"] = ln1_db
+        dw["ln1.weight"], db["ln1.bias"] = ln1_dw, ln1_db
 
         dx = dx_ + dz
         return dx, dw, db
